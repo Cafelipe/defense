e700bb55529c2749bcf6cef41fe86a29f75a2dc0
Tales Ruan <talesruan@gmail.com>
2018-02-08 13:40:57 -0200

    Criando primeiro arquivo de teste.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+print "Hello world"
+
+print "Pessoas que já conseguiram configurar o repositório e o projeto:"
+print "Tales"
+
+print ""
+print "Noobs que ainda não:"
+print "Vini"
+print "Manu"
