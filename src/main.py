99c4770f8746e97ccddb4e0a0a5b59aa817bf551
Vinicius Bevilaqua <vinilaqua@gmail.com>
2018-02-09 00:22:35 -0200

    PRINT -> Python 3
    Adicionados os parenteses nos PRINTS para executar no Python 3

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 # coding=utf-8
-print "Hello world"
+print ("Hello world")
 
-print "Pessoas que já conseguiram configurar o repositório e o projeto:"
-print "Tales"
-print "Vini"
+print ("Pessoas que já conseguiram configurar o repositório e o projeto:")
+print ("Tales")
+print ("Vini")
 
-print ""
-print "Noobs que ainda não:"
-print "Manu"
+print (".")
+print ("Noobs que ainda não:")
+print ("Manu")
