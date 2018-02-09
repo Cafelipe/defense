fbc4e003e44891ddbf820031f807f4570824c2e5
Entravix <entravix@gmail.com>
2018-02-09 09:19:49 -0200

    Import Lib Telethon
    Dá o import na lib do Telethon

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 # coding=utf-8
+from telethon import TelegramClient
+
+api_id = 12345
+api_hash = '0123456789abcdef0123456789abcdef'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
 print ("Hello world")
 
 print ("Pessoas que já conseguiram configurar o repositório e o projeto:")
