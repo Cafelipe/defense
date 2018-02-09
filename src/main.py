60951e818ef0f91c79b98c8f5dc5f60e2e0ac59c
Vinicius Bevilaqua <vinilaqua@gmail.com>
2018-02-08 23:40:14 -0200

    Vini deixou de ser noob

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ print "Hello world"
 
 print "Pessoas que já conseguiram configurar o repositório e o projeto:"
 print "Tales"
+print "Vini"
 
 print ""
 print "Noobs que ainda não:"
-print "Vini"
 print "Manu"
