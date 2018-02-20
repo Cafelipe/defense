0bbf6b77fcc2fec1437c19f6dd714037c71802ea
Tales Ruan <talesruan@gmail.com>
2018-02-20 20:26:24 -0300

    Implementando leitura de status (comando /hero)

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,14 @@
 from telethon import TelegramClient
 from telethon.tl.functions.messages import SendMessageRequest
+from datetime import datetime
 
 import config
 
 import datetime
 import time
 import random
+
+from src.playerStatus import PlayerStatus
 
 
 def forayTime(chatTarget):
@@ -32,6 +35,52 @@ def isWartime(datetime):
 
     return False
 
+# Returns the delta Time between now() and the parameter.
+# Copied from: https://stackoverflow.com/a/14978116
+def delta_time_ms(t):
+   dt = datetime.datetime.now() - t
+   ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+   return ms
+
+
+# Posts /hero and awaits for the player status message
+def getPlayerStatus():
+    client(SendMessageRequest(chatTarget, message="/hero"))
+    print("Posted /hero. Waiting for reply")
+
+    status_timeout = 60*1000  # Timeout in milisseconds
+
+    startTime = datetime.datetime.now()
+
+    done = False
+
+    # Waits for reply
+    while not done and delta_time_ms(startTime) <= status_timeout:
+        time.sleep(1)
+        messages = client.get_message_history(chatTarget, limit=1)
+        done = isHeroReplyMessage(messages[0])
+
+    if done:
+        print("Got reply from the server")
+    else:
+        print("Server did not reply in time.")
+
+
+
+
+
+
+# Returns true if this message is a reply to the /hero command
+# TODO: Make it work (probably using regex)
+def isHeroReplyMessage(message):
+    #
+
+    if message.message.find('PEPE') != -1: ## This line detects if this message is the reply we expect. TODO: Fix it.
+        print("This Message is the one I want: "+message.message)
+    else:
+        print("This is not the message I want "+message.message)
+
+    return message.message.find('PEPE') != -1
 
 # Inicia o client
 client = TelegramClient(config.session_name, config.api_id, config.api_hash)
@@ -42,9 +91,18 @@ except:
     print("Connecting error.")
     quit()
 
-print("Connected.")
+print("Connected to telegram.")
 
 chatTarget = 'chtwrsbot'
+
+# Bot initialization:
+
+print("Starting bot. Getting player status...")
+
+status = PlayerStatus()
+
+getPlayerStatus()
+
 
 # Lazy loop, sorry for that
 while True:
---
+++ src/playerStatus.py
@@ -0,0 +1,11 @@
+# Represents the player data as provided by the commands /me or /hero
+class PlayerStatus:
+    level = 0
+    attack = 0
+    defense = 0
+    stamina = 0
+    gold = 0
+
+
+
+
