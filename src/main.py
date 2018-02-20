9828b554b01089c3d826d7a2562d03feacf62b5e
Vinicius Bevilaqua <vinilaqua@gmail.com>
2018-02-20 20:02:23 -0300

    get_dialogs exemples
    update main.py - get_dialogs exemples

--- src/main.py
+++ src/main.py
@@ -1,19 +1,98 @@
 # coding=utf-8
 from telethon import TelegramClient
+from telethon.utils import get_display_name
+from telethon.tl.functions.messages import SendMessageRequest
+from telethon.tl.types import PeerChannel
+from telethon.tl.types import PeerUser
 
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+import configparser
+import json
+import os
 
-client = TelegramClient('session_name', api_id, api_hash)
+
+def printJSON(var):
+    print(json.dumps(var, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+def returnPeer(dialogo):
+    if dialogo.__class__.__name__ == "Channel":
+        peer = PeerChannel(dialogo.id)
+        return peer
+    elif dialogo.__class__.__name__ == "User":
+        peer = PeerUser(dialogo.id)
+        return peer
+
+
+def carregaChat(dialogo, lim=10):
+    while True:
+        messages = client.get_message_history(dialogo, limit=lim)
+        for x in range(lim - 1, -1, -1):
+            print(str(messages[x].from_id) + ":")
+            print("     " + messages[x].message)
+            print()
+
+        print("------------------------------------------")
+        resp = input('>: ')
+        if resp == "!q":
+            break
+        elif resp == '':
+            None
+        else:
+            client(SendMessageRequest(peer=returnPeer(dialogo), message=resp))
+
+    return
+
+
+# Busca as configurações no config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_id = config["telegramAPI"]['api_id']
+api_hash = config["telegramAPI"]['api_hash']
+
+# Inicia o client
+client = TelegramClient('some_name', api_id, api_hash)
 client.start()
 
-print ("Hello world")
+# Variaveis de controle
+total_resultados = 10
+isLoop = True
+i = None
 
-print ("Pessoas que já conseguiram configurar o repositório e o projeto:")
-print ("Tales")
-print ("Vini")
-print ("Manu")
+while isLoop:
 
-print (".")
-print ("Noobs que ainda não:")
-print ("")
+    # Busca os ultimos N conversar ativas
+    dialog = client.get_dialogs(limit=total_resultados)
+    os.system('cls')
+    print("------------------------------------------")
+    print("Listando ultimas " + str(total_resultados) + " conversas:")
+
+    for x in range(0, int(total_resultados)):
+        print(str(x) + ' - ' + get_display_name(dialog[x].entity))
+
+    print("------------------------------------------")
+    print('  Digite o código da conversa para selecionar.')
+    print('  !Lim: Alterar limite de conversas listadas.')
+    print('  !S: Pesquisar contato por usuário.')
+    print('  !Q: Finaliza.')
+    print("------------------------------------------")
+    i = input('Entre com o CHAT ID ou um comando: ')
+
+    if i == '!Q':
+        isLoop = False
+
+    elif i == '!S':
+        usuario = client.get_entity(input('Pesquisar por: '))
+        print('Nome: ' + str(usuario.first_name) + ' ' + str(usuario.last_name))
+        print('Usuário: ' + str(usuario.username))
+        print('Chat ID: ' + str(usuario.id))
+
+    elif i == '!Lim':
+        total_resultados = input('Digite o nome limite de conversas: ')
+
+    else:
+        os.system('cls')
+        print("Selecionado " + get_display_name(dialog[int(i)].entity) + " - ID: " + str(dialog[int(i)].entity.id))
+        print()
+        carregaChat(dialog[int(i)].entity)
+
+    print()
