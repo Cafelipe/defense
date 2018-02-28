96aa80452860d7f17f4687f45dd093d226cf446e
Vinicius Bevilaqua <vinilaqua@gmail.com>
2018-02-28 14:16:35 -0300

    UTC Timezone
    Now, working in any timezone.

--- src/bot.py
+++ src/bot.py
@@ -3,9 +3,10 @@ from telethon.tl.functions.messages import SendMessageRequest
 
 import config
 
-import datetime
+from datetime import datetime
 import time
 import random
+
 
 
 def forayTime(chatTarget):
@@ -24,7 +25,8 @@ def defenseTime(chatTarget):
 
 
 def isWartime(datetime):
-    schedule = [0, 4, 8, 12, 16, 20]
+    # UTC
+    schedule = [7, 15, 23]
 
     for val in schedule:
         if datetime.hour == val and datetime.minute >= random.randint(54, 58):
@@ -32,6 +34,9 @@ def isWartime(datetime):
 
     return False
 
+#TOP KEK FUNCTION
+def getTimezone():
+    return datetime.now().hour - datetime.utcnow().hour
 
 # Inicia o client
 client = TelegramClient(config.session_name, config.api_id, config.api_hash)
@@ -43,14 +48,17 @@ except:
     quit()
 
 print("Connected.")
+print(str(time.tzname[0])+" / Timezone: "+str(getTimezone()))
 
+#Chatwars Channel
 chatTarget = 'chtwrsbot'
 
 # Lazy loop, sorry for that
 while True:
 
     # Get current time.
-    timeNow = datetime.datetime.now()
+    timeNow = datetime.now()
+    timeNowUTC = datetime.utcnow()
 
     # Get target last message, I know its fallible, but who cares?
     messages = client.get_message_history(chatTarget, limit=1)
@@ -62,7 +70,7 @@ while True:
         forayTime(chatTarget)
 
     # Check Wartime
-    if isWartime(timeNow):
+    if isWartime(timeNowUTC):
         print(str(timeNow) + " - Defense time!")
         isDefended = True
         defenseTime(chatTarget)
